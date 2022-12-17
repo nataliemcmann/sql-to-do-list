ba29acd4b7b6fe8e5cd9c999b7087bb00f4853a5
Natalie McMann <nmcmann110@gmail.com>
2022-12-17 11:06:57 -0600

    set up pg/pool and server boilerplate

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -1,1 +1,26 @@
-//make a pg pool in here then export it
+//import pg library
+//this is to coneect the database with the server. 
+const pg = require('pg');
+
+const Pool = pg.Pool;
+
+const pool = new Pool({
+	database: 'weekend-to-do-app', //name of database
+	host: 'localhost', //where the database is
+	port: 5432, //this is the default port for postgres
+	max: 10, //how many queries the database can handle at one time
+	idleTimeoutMillis: 30000 //30 seconds to try to connect, else cancel connection
+});
+
+//turn pool on
+pool.on('connect', () => {
+	console.log('Postgresql connected');
+});
+
+//error message to return if any occur
+pool.on('error', (error) => {
+	console.log('Error with postgres pool', error)
+});
+
+//export pool
+module.exports = pool;
--- server/server.js
+++ server/server.js
@@ -1,1 +1,29 @@
 //make a server in here
+//import express
+const express = require('express');
+//import body-parser
+const bodyParser = require('body-parser');
+//import taskRouter
+const taskRouter = require('./router/taskRouter')
+//import pool
+const pool = require('./module/pool.js');
+
+//make express server
+const app = express();
+//run server on port 5000
+const PORT = process.env.PORT || 5000;
+
+//teach server to read JSON
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json())
+
+//upon server access, send the public folder
+app.use(express.static('server/public'));
+
+// ROUTES
+app.use('/tasks', taskRouter)
+
+// Start listening for requests on a specific port
+app.listen(PORT, () => {
+  console.log('listening on port', PORT);
+});
