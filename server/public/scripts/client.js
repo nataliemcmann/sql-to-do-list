af133f6b6daa41c32d0b8d255cc1b2ded8d678fd
Natalie McMann <nmcmann110@gmail.com>
2022-12-17 12:25:26 -0600

    get http request works on client side, added function to remove timestamp from date

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,1 +1,51 @@
-//have jquery do things in here
+//confirm js script sourced
+// console.log('Ready, Set...');
+
+//call jQuery
+$(document).ready(onReady)
+
+//functions to run upon DOM render
+function onReady(){
+    // console.log('GO!');
+    getAndRenderTasks();
+}
+
+//get request
+function getAndRenderTasks(){
+    console.log('getting tasks');
+    $.ajax({
+        method: 'GET',
+        url: '/tasks'
+    })
+    .then((res) => {
+        renderTasks(res);
+    })
+    .catch((err) => {
+        console.log('GET http request could not be completed', err);
+    })
+}
+
+//render tasks
+function renderTasks(array){
+    $('#taskList').empty();
+    for (let item of array){
+        $('#taskList').append(`
+        <tr data-id=${item.id}>
+            <td>${removeTime(item.date)}</td>
+            <td>${item.freq}</td>
+            <td>${item.task}</td>
+            <td>${item.complete}</td>
+        </tr>
+        `)
+    }
+}
+
+//reformat sql date function 
+function removeTime(SQLdate){
+    let newDate = '';
+    for (let i = 0; i < 10; i++){
+        newDate += SQLdate[i];
+    }
+    return newDate;
+}
+//find a way to make the date look nicer 
